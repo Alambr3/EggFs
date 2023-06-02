919b70dfa91140b4ee7cf79969263a4360b9be86
Alambr3 <agustin.retamar92@gmail.com>
2023-06-02 20:07:05 -0300

    correccion nombre

---
+++ Modulos 3 POO/Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,18 @@
+package ejercicio2;
+import ejercicio2.entidades.Circunferencia;
+import ejercicio2.servicios.ServicioCircunferencia;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+       Scanner leer = new Scanner(System.in).useDelimiter("\n"); 
+       
+       ServicioCircunferencia sc = new ServicioCircunferencia();
+       Circunferencia c1 = sc.crearCircunferencia();
+       
+       sc.area(c1);
+       sc.perimetro(c1);
+    }
+
+}
---
+++ Modulos 3 POO/Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,19 @@
+package ejercicio4;
+
+import ejercicio4.entidades.Rectangulo;
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in).useDelimiter("\n");
+
+        Rectangulo r1 = new Rectangulo();
+        r1.crearRectangulo();
+        System.out.println("Area");
+        System.out.println(r1.calcularSuperficie());
+        System.out.println("Perimetro");
+        System.out.println(r1.calcularPerimetro());
+    }
+
+}
